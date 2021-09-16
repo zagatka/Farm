2099513061916cef0cd3488a42a130605083a2a5
Agata <stanek.agatka@gmail.com>
2021-09-16 15:20:54 +0200

    added data.sql with same testing data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into ANIMAL(id, type, name, age)
+values (1, dog, "Kubus", 5), (2, dog, "Reksio", 2), (3, dog, "Max", 8);
+insert into PLANT(id, type, area)
+values(1, grass, 1.5), (2, oat, 2.9), (3, corn, 0.5), (4, wheat, 9.0);
