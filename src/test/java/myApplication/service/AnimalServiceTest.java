36c0817dad8e554dd88c8ae1796fc2d3453552ba
Agata <stanek.agatka@gmail.com>
2021-09-16 15:21:24 +0200

    added tests and constants

--- src/test/java/myApplication/service/AnimalServiceTest.java
+++ src/test/java/myApplication/service/AnimalServiceTest.java
@@ -2,8 +2,10 @@ package myApplication.service;
 
 import myApplication.dto.AnimalDto;
 import myApplication.entity.AnimalEntity;
+import myApplication.exception.NoElementException;
 import myApplication.repository.AnimalRepository;
 import myApplication.service.Impl.AnimalServiceImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,8 +13,13 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 @ExtendWith(MockitoExtension.class)
 class AnimalServiceTest {
+
+    private static final long ANIMAL_ID_NOT_IN_DATABASE = 123L;
+    private static final long ANIMAL_ID = 1L;
 
     @Mock
     private AnimalRepository animalRepository;
@@ -31,4 +38,43 @@ class AnimalServiceTest {
         //t
         Mockito.verify(animalRepository).save(Mockito.any());
     }
+
+    @Test
+    void shouldDeleteAnimalThrowExceptionForNoAnimalWithGivenIdInDatabase() {
+        //g
+
+        //w
+
+        //t
+        Assertions.assertThrows(NoElementException.class, () -> animalService.deleteAnimal(ANIMAL_ID_NOT_IN_DATABASE));
+    }
+
+    @Test
+    void shouldDeleteAnimal() throws NoElementException {
+        //g
+        AnimalEntity animalEntity = new AnimalEntity();
+        Optional<AnimalEntity> animalEntityOptional =  Optional.of(animalEntity);
+        Mockito.when(animalRepository.findById(Mockito.any())).thenReturn(animalEntityOptional);
+
+        //w
+        animalService.deleteAnimal(ANIMAL_ID);
+
+        //t
+        Mockito.verify(animalRepository).delete(Mockito.any());
+
+    }
+
+    @Test
+    void shouldFindAnimalById() throws NoElementException {
+        //g
+
+
+        //w
+        animalService.findAnimalById(ANIMAL_ID);
+
+        //t
+        Mockito.verify(animalRepository).findById(Mockito.any());
+
+    }
+
 }
