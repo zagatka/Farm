1b95c8ab19a64e2b1abbcbe148997e93140e3de1
Agata <stanek.agatka@gmail.com>
2021-09-27 12:21:13 +0200

    added test data

---
+++ src/test/resources/data.sql
@@ -0,0 +1,4 @@
+insert into ANIMAL(id, type, name, age, version)
+values (101, 'DOG', 'Kubus', 5, 1), (102, 'DOG', 'Reksio', 2, 1), (103, 'DOG', 'Max', 8, 1);
+insert into PLANT(id, type, area, version)
+values(101, 'GRASS', 1.5, 1), (102, 'OAT', 2.9, 1), (103, 'CORN', 0.5, 1), (104, 'WHEAT', 9.0, 1);
